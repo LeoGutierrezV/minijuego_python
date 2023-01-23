3c94bfdcfe6871991323c55ac9d18b61cf2036fa
LeoGutierrezV <81544580+LeoGutierrezV@users.noreply.github.com>
2023-01-22 20:18:56 -0500

    Update adivina_el_numero_computadora.py

--- minijuego_adivina el numero/adivina_el_numero_computadora.py
+++ minijuego_adivina el numero/adivina_el_numero_computadora.py
@@ -1,11 +1,5 @@
-"""
-Proyecto Básico de Python (Adivina el Número (Computadora)).
-Basado en el proyecto de: Kylie Ying (@kylieyying). 
-Versión en Español con Modificaciones: Estefania Cassingena Navone (@EstefaniaCassN).
-"""
 import random
 
-# La computadora debe adivinar el número seleccionado por el jugador.
 def adivina_el_número_computadora(x):
 
     print("============================")
@@ -18,16 +12,12 @@ def adivina_el_número_computadora(x):
     límite_superior = x
     respuesta = ""
 
-    # Miestras el usuario no indique que la respuesta es correcta,
-    # continuar el proceso.
     while respuesta != "c":
-        # Generar predicción
         if límite_inferior != límite_superior:
             predicción = random.randint(límite_inferior, límite_superior)
         else:
-            predicción = límite_inferior  # también podría ser límite_superior porque los límites son iguales.
+            predicción = límite_inferior 
         
-        # Obtener respuesta del usuario
         respuesta = input(f"Mi predicción es {predicción}. Si es muy alta, ingresa (A). Si es muy baja, ingresa (B). Si es correcta ingresa (C) ").lower()
         
         if respuesta == "a":
@@ -35,7 +25,7 @@ def adivina_el_número_computadora(x):
         elif respuesta == "b":
             límite_inferior = predicción + 1
 
-    print(f"¡Siii! La computadora adivinó tu número correctamente: {predicción}")
+    print(f"La computadora adivinó tu número correctamente: {predicción}")
 
 
 adivina_el_número_computadora(10)
